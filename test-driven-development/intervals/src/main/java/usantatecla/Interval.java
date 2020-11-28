3d04bb4bdaeca8c3a7139ba202917409c8eade4b
franrobles8 <franrobles8@gmail.com>
2020-11-28 17:56:24 +0100

    Changed method isIntersected() to use include() and check also min values and fix test

--- test-driven-development/intervals/src/main/java/usantatecla/Interval.java
+++ test-driven-development/intervals/src/main/java/usantatecla/Interval.java
@@ -16,7 +16,7 @@ public class Interval {
 	}
 
 	public boolean isIntersected(Interval interval) {
-		return this.max.isWithin(interval.min.value) || this.max.isWithin(interval.max.value);
+		return this.include(interval.min.value) || this.include(interval.max.value);
 	}
 
 	@Override
