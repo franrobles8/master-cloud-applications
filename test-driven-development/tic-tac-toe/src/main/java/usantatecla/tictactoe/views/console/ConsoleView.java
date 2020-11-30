ac5552f988a9c3ffe78ab27de4e7128b308a63fc
franrobles8 <franrobles8@gmail.com>
2020-11-30 12:33:01 +0100

    [Facade pattern] Added first draft of wrapper class ConsoleView with dummy methods

---
+++ test-driven-development/tic-tac-toe/src/main/java/usantatecla/tictactoe/views/console/ConsoleView.java
@@ -0,0 +1,19 @@
+package usantatecla.tictactoe.views.console;
+
+public class ConsoleView extends usantatecla.tictactoe.views.View {
+    @Override
+    protected void start() {
+        // To do
+    }
+
+    @Override
+    protected void play() {
+        // To do
+    }
+
+    @Override
+    protected boolean isResumed() {
+        // To do
+        return false;
+    }
+}
