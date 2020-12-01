61f66c40ddd0fbfa5862f7fb32c0ef69c0b4e582
g4lk <pd.parla@alumnos.urjc.es>
2020-12-01 21:06:02 +0100

    delete unused import

--- test-driven-development/tic-tac-toe/src/test/java/usantatecla/tictactoe/views/console/CoordinateViewTest.java
+++ test-driven-development/tic-tac-toe/src/test/java/usantatecla/tictactoe/views/console/CoordinateViewTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import usantatecla.tictactoe.models.Coordinate;
 import usantatecla.utils.Console;
