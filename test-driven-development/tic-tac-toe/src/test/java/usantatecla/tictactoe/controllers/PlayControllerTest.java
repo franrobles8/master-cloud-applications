d9e8d063a0b903530630ceb7e42b6d83949dd271
g4lk <pd.parla@alumnos.urjc.es>
2020-12-01 19:12:11 +0100

    [Double dispatch] Create empty test for play controller

---
+++ test-driven-development/tic-tac-toe/src/test/java/usantatecla/tictactoe/controllers/PlayControllerTest.java
@@ -0,0 +1,15 @@
+package usantatecla.tictactoe.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+public class PlayControllerTest {
+    
+    
+	@InjectMocks
+    private PlayController playController;
+    
+    @Test
+	void testGivenPlayControllerWhenAcceptThenControllersVisitorVisit() {
+	}
+}
